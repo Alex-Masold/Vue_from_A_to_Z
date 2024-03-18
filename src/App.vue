b6d4f93a58f63ec1f9213ffddb10b34723638792
Kob <sidorowsacha2005@gmail.com>
2024-03-18 22:08:43 +0700

    !

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class ="app">
+        <post-form/>
+        <post-list/>
+    </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+
+    export default {
+        components:
+        {
+            PostForm, 
+            PostList
+        },
+        data() {
+            return {
+                posts: [
+                    {
+                        id: 1, 
+                        title: 'Post 1', 
+                        body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa autem ratione eius, aperiam necessitatibus sunt earum quas impedit. Nulla molestiae ipsam delectus facilis veritatis sit veniam atque ut a nostrum.'
+                    },
+                    {
+                        id: 2,
+                        title: 'Post 2',
+                        body: 'Doloribus dicta autem excepturi similique culpa natus non, quidem voluptatibus atque aspernatur voluptate incidunt magnam veniam nam nemo repellat accusamus nulla quis quasi fugiat. Eos odit nostrum molestiae illum porro?'
+                    },
+                    {
+                        id: 3,
+                        title: 'Post 3',
+                        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, ipsa voluptatibus itaque consequuntur quos possimus accusantium reprehenderit repellendus ipsam sint cupiditate, consectetur dolor in blanditiis deleniti ducimus rem nostrum soluta.'
+                    },
+                ],
+                title: '',
+                body: ''
+            }
+        },
+        methods: {
+            CreatePost()
+            {
+                const newPost = {
+                    id: Date.now(),
+                    title: this.title,
+                    body: this.body,
+                }
+                this.posts.push(newPost);
+
+                this.title = '';
+                this.body = '';
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .app{
+        padding: 20px;
+    }
+    
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="post" v-for="post in posts">
+        <div>
+            <strong>Название: </strong>
+            <i>{{ post.title }}</i>
+        </div>
+        <div>
+            <strong>Описание: </strong>
+            {{ post.body }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+    .post {
+            padding: 0.5em;
+            border: 3px solid teal;
+            margin-top: 15px;
+        }
+</style>
